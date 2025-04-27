f7d2511370cef8672849dc4c30ea6a4c90292719
Juzi-wei <SharonuCohnw31@gmail.com>
2025-04-28 01:02:11 +0800

    Update config.js

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -13,9 +13,9 @@ const CONFIG = {
   STARTER_HERO_TITLE_1: '装配式机电产品', // 英雄区文字
   STARTER_HERO_TITLE_2: '成品支架、抗震支架、装配式预制模块、预制构件', // 英雄区文字
   // 英雄区两个按钮，如果TEXT留空则隐藏按钮
-  STARTER_HERO_BUTTON_1_TEXT: '开始体验', // 英雄区按钮
+  STARTER_HERO_BUTTON_1_TEXT: '最新公告', // 英雄区按钮
   STARTER_HERO_BUTTON_1_URL:
-    'https://noteforms.com/forms/4vx4pd', // 英雄区按钮
+    'https://chanyeyuan.amreal.cc/about', // 英雄区按钮
   STARTER_HERO_BUTTON_2_TEXT: '', // 英雄区按钮
   STARTER_HERO_BUTTON_2_URL: '', // 英雄区按钮
   STARTER_HERO_BUTTON_2_ICON: '', // 英雄区按钮2的图标，不需要则留空
